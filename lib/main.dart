966f5e81683aa5c6426a3a788a4602be62bd4181
Jacob Moura <tecleigo@gmail.com>
2019-05-17 18:38:40 -0300

    flutter WhatsApp

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter WhatsApp',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         dividerColor: Color(0xFFBDC1C3),
